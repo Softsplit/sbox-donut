b3eb86cac00ee875d0c42038111721286eb7d9df
Asphaltian <amirulhh2009@gmail.com>
2024-03-03 18:04:04 +0800

    Add playerlist

--- code/NetworkManager.cs
+++ code/NetworkManager.cs
@@ -1,14 +1,30 @@
 using System.Collections.Generic;
 using Sandbox;
 using System.Linq;
+using Sandbox.Network;
+using System.Threading.Tasks;
+using System;
+namespace Donut;
 
 public sealed class NetworkManager : Component, Component.INetworkListener
 {
     public static NetworkManager Instance { get; private set; }
 
+    /// <summary>
+	/// Create a server (if we're not joining one)
+	/// </summary>
+	[Property] public bool StartServer { get; set; } = true;
+
+	/// <summary>
+	/// The prefab to spawn for the player to control.
+	/// </summary>
+	[Property] public GameObject PlayerPrefab { get; set; }
+
     public List<Connection> Connections = new();
     public Connection Host = null;
     [Sync] public long HostSteamId { get; set; }
+
+    public List<PlayerController> Players => GameManager.ActiveScene.Components.GetAll<PlayerController>(FindMode.EnabledInSelfAndDescendants).ToList();
 
     protected override void OnAwake()
     {
@@ -16,6 +32,19 @@ public sealed class NetworkManager : Component, Component.INetworkListener
 
         Instance = this;
     }
+
+    protected override async Task OnLoad()
+	{
+		if (Scene.IsEditor)
+			return;
+
+		if (StartServer && !GameNetworkSystem.IsActive)
+		{
+			LoadingScreen.Title = "Creating Lobby";
+			await Task.DelayRealtimeSeconds(0.1f);
+			GameNetworkSystem.CreateLobby();
+		}
+	}
 
     /// <summary>
     /// A client is fully connected to the server. This is called on the host.
@@ -31,17 +60,52 @@ public sealed class NetworkManager : Component, Component.INetworkListener
             Host = channel;
             HostSteamId = (long)channel.SteamId;
         }
+
+        if (PlayerPrefab is null)
+			return;
+
+        SpawnPlayer(channel);
     }
 
-    public void OnDisconnected( Connection channel )
+	public void SpawnPlayer( Connection channel )
+	{
+		var startLocation = Transform.World;
+
+		startLocation.Scale = 1;
+
+		// Spawn this object and make the client the owner
+		var player = PlayerPrefab.Clone( startLocation, name: $"Player - {channel.DisplayName}" );
+		player.NetworkSpawn( channel );
+
+		var playerController = player.Components.Get<PlayerController>( FindMode.EverythingInSelfAndDescendants );
+		playerController.SetName( channel.DisplayName );
+	}
+
+	public void OnDisconnected( Connection channel )
     {
+        foreach (var player in Players)
+		{
+			if (player.SteamId == (long)channel.SteamId)
+			{
+				player.GameObject.Destroy();
+			}
+		}
+
         Connections.Remove( channel );
     }
 
     public void OnBecameHost( Connection previousHost )
     {
+        foreach (var player in Players)
+		{
+			if (player.SteamId == (long)previousHost.SteamId)
+			{
+				player.GameObject.Destroy();
+			}
+		}
+
         Host = Connections.FirstOrDefault( x => x.SteamId == (ulong)Game.SteamId );
-        HostSteamId = (long)Game.SteamId;
+        HostSteamId = Game.SteamId;
 
         Log.Info( "You are now the host!" );
     }
---
+++ code/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+using Sandbox;
+namespace Donut;
+
+public sealed class PlayerController : Component
+{
+	public static PlayerController Local => NetworkManager.Instance.Players.FirstOrDefault(x => x.SteamId == Game.SteamId);
+	
+	[Sync] public long SteamId { get; set; }
+
+	protected override void OnUpdate()
+	{
+		if (!IsProxy)
+		{
+			SteamId = Game.SteamId;
+		}
+	}
+
+	[Broadcast]
+	public void SetName(string name)
+	{
+		GameObject.Name = name;
+	}
+}
